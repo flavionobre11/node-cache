5e1414c6d0c1eed0042997fce64f71935746cf6c
Flavio Nobre <flavionobre11@gmail.com>
2021-12-25 18:51:58 -0300

    feat: finish test to redis connector

--- doc/usecases/1-redis-connector.md
+++ doc/usecases/1-redis-connector.md
@@ -1,10 +1,8 @@
 # Redis Connector
 
 ## 1 - Ao conectar com sucesso deverá retornar as informações da conexao
+## 2 - Ao falhar, deverá retornar uma exception
 
-## 2 - Devera emitir um evento "ready" ao conectar com sucesso no redis.
 
-## 3 - Ao falhar, deverá retornar uma exception
 
-## 4 - Tambem ao falhar, deverá emitir um evento de falha
 
--- tests/e2e/drivers/redis/redis-driver.spec.ts
+++ tests/e2e/drivers/redis/redis-driver.spec.ts
@@ -1,5 +1,27 @@
-describe("Redis driver", () => {
-  it('should be ok', () => {
-    expect(true).toBeTruthy()
+import { createClient, RedisClientOptions } from 'redis'
+
+class RedisConnector {
+  
+  static async connect(configs?: RedisConnectorConfigs){
+    const client = createClient(configs)
+    await client.connect().catch(error => {
+      throw new Error('redis connection error')
+    })
+    return client
+  }
+}
+
+
+interface RedisConnectorConfigs extends Omit<RedisClientOptions<never, Record<string, never>>, "modules"> {}
+
+
+describe("Redis Connector", () => {
+
+  it('should connect successfully passing correct configs', async () => {
+    return expect(RedisConnector.connect()).resolves.toBeDefined()
+  })
+
+  it('should connect error passing wrong configs', async () => {
+    return expect(RedisConnector.connect({url: 'localhost:80'})).rejects.toThrowError()
   })
 })
