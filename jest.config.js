0fe3e74b9e728edf2c8fc63d9521f4e2e1dfe1e2
Flavio Nobre <flavionobre11@gmail.com>
2021-12-25 23:41:36 -0300

    add module mapper for path alias

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  bail: true,
   preset: 'ts-jest',
   clearMocks: true,
-  coverageProvider: "v8",
   setupFiles: ['dotenv/config'],
   testEnvironment: "node",
   testMatch: ["**/tests/**/*.(test|spec).ts"],
   testPathIgnorePatterns: [
     "/node_modules/"
   ],
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  }
 };
