8260a6d740ae49f1f1edb7010a804ddd2dcb43be
Flavio Nobre <flavionobre11@gmail.com>
2021-12-27 13:06:02 -0300

    feat: finish main flow tests

--- doc/usecases/1-insert.md
+++ doc/usecases/1-insert.md
@@ -3,6 +3,8 @@
 ## Main Flow
 
 1. deverá cadastrar um chave-valor
+2. deverá ser possível cadastrar um valor com expiração em milliseconds
+3. deverá ser possível cadastrar um valor com expiração em uma data (i.e. new Date())
 
 ## Alternative flow: a chave deve ser valida
 
@@ -11,3 +13,7 @@
 ## Alternative flow: o valor deve ser válido
 
 1. throw exception: "value should be not null"
+
+## Alternative flow: a expiração deve ser um number ou date
+
+1. throw exception: "exp should be type of number or date instance"
--- tests/unity/domain/usecase/insert-usecase.spec.ts
+++ tests/unity/domain/usecase/insert-usecase.spec.ts
@@ -3,37 +3,59 @@ import { RedisClient } from '@/domain/adapters/redis/redis.adapter';
 class InsertValue {
   constructor(private readonly redisDriver: RedisDriver) {}
 
-  async perform(key: string, value: string) {
-    return await this.redisDriver.set(key, value);
+  async perform(key: string, value: string, options?: { exp: number | Date }) {
+    return await this.redisDriver.set(key, value, options);
   }
 }
 
 class RedisDriverMock implements RedisDriver {
   constructor(private readonly redisClient: RedisClient) {}
 
-  async set(key: string, value: string) {
-    const result = await this.redisClient.set(key, value);
+  async set(key: string, value: string, options?: { exp: number | Date }) {
+    const opt = {};
+    let expires = undefined;
+
+    if (typeof options?.exp === 'number') {
+      Reflect.set(opt, 'PX', options?.exp); // milli unix
+      expires = new Date(Date.now() + options?.exp).toISOString();
+    } else if (options?.exp instanceof Date) {
+      Reflect.set(
+        opt,
+        'EXAT',
+        Number((options?.exp.getTime() / 1000).toFixed(0)),
+      ); // date to unix
+      expires = new Date(options?.exp).toISOString();
+    }
+
+    const result = await this.redisClient.set(key, value, opt);
     if (!result) throw new Error('value not set');
+
     return {
       key,
       value,
+      expires,
     };
   }
 }
 
 const RedisClientMock = {
-  set: jest
-    .fn()
-    .mockImplementation((key, value) => (key && value ? 'OK' : null)),
+  set: jest.fn().mockImplementation((key, value, options?) => {
+    return key && value ? 'OK' : null;
+  }),
 } as Pick<RedisClient, 'set'>;
 
 type RedisDriveSetResponse = {
   key: string;
   value: string;
+  expires?: string;
 };
 
 interface RedisDriver {
-  set(key: string, value: string): Promise<RedisDriveSetResponse>;
+  set(
+    key: string,
+    value: string,
+    options?: { exp: number | Date },
+  ): Promise<RedisDriveSetResponse>;
 }
 
 const makeSut = () => {
@@ -55,5 +77,28 @@ describe('InsertValue Usecase', () => {
       key: key,
       value: value,
     });
+  });
+
+  it('should be possible insert a value with deadline in milliseconds', async () => {
+    const key = 'any_key:expires:30seconds';
+    const value = 'any_value';
+    const expiresIn = 30 * 1000;
+    const { sut } = makeSut();
+    const result = await sut.perform(key, value, { exp: expiresIn });
+    expect(result).toHaveProperty('expires');
+    expect(typeof result.expires).toBe('string');
+  });
+
+  it('should be possible insert a value with deadline on date format', async () => {
+    const key = 'any_key:expires:1day';
+    const value = 'any_value';
+    const expiresIn = new Date(Date.now() + 24 * 60 * 60 * 1000);
+    const { sut } = makeSut();
+    const result = await sut.perform(key, value, {
+      exp: expiresIn,
+    });
+    expect(result).toHaveProperty('expires');
+    expect(typeof result.expires).toBe('string');
+    expect(result.expires).toBe(expiresIn.toISOString());
   });
 });
