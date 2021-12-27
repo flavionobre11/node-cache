71f03152c9e67774945a0d98436d514168adf815
Flavio Nobre <flavionobre11@gmail.com>
2021-12-27 16:47:34 -0300

    refact: add usecase to redis driver

--- src/domain/adapters/redis/redis-driver.ts
+++ src/domain/adapters/redis/redis-driver.ts
@@ -1,32 +1,11 @@
 import { CacheDriver } from '@/domain/models/cache-drive.model';
 import { RedisClient } from './redis.adapter';
+import SetCommand, { RedisDriverSetOptions } from './usecases/set.usecase';
 
 export default class RedisDriver implements CacheDriver {
   constructor(private readonly redisClient: RedisClient) {}
 
-  async set(key: string, value: string, options?: { exp: number | Date }) {
-    const opt = {};
-    let expires = undefined;
-
-    if (typeof options?.exp === 'number') {
-      Reflect.set(opt, 'PX', options?.exp); // milli unix
-      expires = new Date(Date.now() + options?.exp).toISOString();
-    } else if (options?.exp instanceof Date) {
-      Reflect.set(
-        opt,
-        'EXAT',
-        Number((options?.exp.getTime() / 1000).toFixed(0)),
-      ); // date to unix
-      expires = new Date(options?.exp).toISOString();
-    }
-
-    const result = await this.redisClient.set(key, value, opt);
-    if (!result) throw new Error('value not set');
-
-    return {
-      key,
-      value,
-      expires,
-    };
+  async set(key: string, value: string, options?: RedisDriverSetOptions) {
+    return await SetCommand.perform(this.redisClient, key, value, options);
   }
 }
---
+++ src/domain/adapters/redis/usecases/set.usecase.ts
@@ -0,0 +1,62 @@
+import { RedisClient } from '../redis.adapter';
+
+export type RedisDriverSetOptions = { exp: number | Date };
+
+export default class SetCommand {
+  static async perform(
+    redisClient: RedisClient,
+    key: string,
+    value: string,
+    options?: RedisDriverSetOptions,
+  ) {
+    const { parsedOptions, expires } = SetCommand.parseOptions(options);
+    const result = await redisClient.set(key, value, parsedOptions);
+    if (!result) throw new Error('value not set');
+
+    return {
+      key,
+      value,
+      expires,
+    };
+  }
+
+  private static parseOptions(options?: RedisDriverSetOptions) {
+    const parsedOptions = {};
+    let expires = undefined;
+    if (!options)
+      return {
+        parsedOptions,
+        expires,
+      };
+
+    const OPTIONS_TO_PARSE = {
+      exp: (value) => {
+        if (typeof value === 'number') {
+          Reflect.set(parsedOptions, 'PX', value); // milli unix
+          expires = new Date(Date.now() + value).toISOString();
+        } else if (value instanceof Date) {
+          Reflect.set(
+            parsedOptions,
+            'EXAT',
+            Number((value.getTime() / 1000).toFixed(0)),
+          ); // date to unix
+          expires = new Date(value).toISOString();
+        }
+      },
+    } as {
+      [key in keyof Required<RedisDriverSetOptions>]: (
+        param: RedisDriverSetOptions[key],
+      ) => void;
+    };
+
+    const keys = Object.keys(
+      options,
+    ) as unknown as (keyof RedisDriverSetOptions)[];
+
+    keys.forEach((key) => OPTIONS_TO_PARSE[key](Reflect.get(options, key)));
+    return {
+      parsedOptions,
+      expires,
+    };
+  }
+}
