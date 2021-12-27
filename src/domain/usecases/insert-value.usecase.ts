57a9f80eb508dfd93018288ea0cec614d74cb689
Flavio Nobre <flavionobre11@gmail.com>
2021-12-27 14:15:25 -0300

    refact: rename redisDriver to cacheDriver

--- src/domain/usecases/insert-value.usecase.ts
+++ src/domain/usecases/insert-value.usecase.ts
@@ -1,13 +1,13 @@
 import { CacheDriver } from '../models/cache-drive.model';
 
 export default class InsertValue {
-  constructor(private readonly redisDriver: CacheDriver) {}
+  constructor(private readonly cacheDriver: CacheDriver) {}
 
   async perform(key: string, value: string, options?: { exp: number | Date }) {
     if (!key || typeof key !== 'string')
       throw new Error('key should be string and not null');
     if (!value || typeof value !== 'string')
       throw new Error('value should be string and not null');
-    return await this.redisDriver.set(key, value, options);
+    return await this.cacheDriver.set(key, value, options);
   }
 }
