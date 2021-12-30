ee40e76cebb317dd20f488ab8eed2ed71124b235
Flavio Nobre <flavionobre11@gmail.com>
2021-12-30 17:23:41 -0300

    test: add grpc module tests

--- tests/unity/main/factories/grpc-module.spec.ts
+++ tests/unity/main/factories/grpc-module.spec.ts
@@ -1,54 +1,29 @@
-// export default interface GRPCModule {
-//     packageDefinition: PackageDefinition;
-//     initProtoServices(gRPCServer: Server): void;
-//   }
-
+import GRPCModule from '@/main/factories/grpc-module.factory';
 import { ProtoGrpcType } from '@/tests/data/mocks/proto/proto-types/test';
-import { loadPackageDefinition } from '@grpc/grpc-js';
-import { Options, loadSync } from '@grpc/proto-loader';
+import { Server } from '@grpc/grpc-js';
 import { resolve } from 'path';
 
-interface GRPCModuleConfigs {
-  filePath: string;
-  pdOptions: Options; // path definition options
-}
-
-class GRPCModule<T> {
-  public packageDefinition;
-  public proto;
-
-  constructor(private readonly configs: GRPCModuleConfigs) {
-    this.packageDefinition = loadSync(
-      this.configs.filePath,
-      this.configs.pdOptions,
-    );
-    this.proto = loadPackageDefinition(this.packageDefinition) as unknown as T;
-  }
-}
+const makeSut = () => {
+  const gRPCServer = new Server();
+  const testProtoModule = new GRPCModule<ProtoGrpcType>(gRPCServer, {
+    filePath: resolve(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      'data',
+      'mocks',
+      'proto',
+      'test.proto',
+    ),
+  });
+  return { sut: testProtoModule };
+};
 
 describe('GRPC Module Factory', () => {
-  it('should load proto definitions', async () => {
-    const testProtoModule = new GRPCModule<ProtoGrpcType>({
-      filePath: resolve(
-        __dirname,
-        '..',
-        '..',
-        '..',
-        'data',
-        'mocks',
-        'proto',
-        'test.proto',
-      ),
-      pdOptions: {
-        keepCase: true,
-        longs: String,
-        enums: String,
-        defaults: true,
-        oneofs: true,
-      },
-    });
-
-    console.log(testProtoModule.proto);
-    expect(testProtoModule.proto).toBeDefined();
+  it('should load proto definitions', () => {
+    const { sut } = makeSut();
+    console.log(sut.proto);
+    expect(sut.proto).toBeDefined();
   });
 });
