c07956541ab9e89f36cc3f2f05b10ba9d0aa1a0b
Flavio Nobre <flavionobre11@gmail.com>
2022-01-08 17:45:29 -0300

    refactor: add redis uri connection to redis client factory

--- src/main/factories/redis-client.factory.ts
+++ src/main/factories/redis-client.factory.ts
@@ -4,7 +4,7 @@ export default class RedisClientFactory {
   private static client = null as unknown as RedisClient;
   static async getClient(){
     if(RedisClientFactory.client) return RedisClientFactory.client 
-    const redisClient = await new RedisConnector().connect()
+    const redisClient = await new RedisConnector({url: process.env.REDIS_URI}).connect()
     RedisClientFactory.client = redisClient
     return RedisClientFactory.client
   }
